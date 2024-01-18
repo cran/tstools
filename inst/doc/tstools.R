c5df636a1a029f0da080d46b0c8ed3f59b5bfd52
Stéphane Bisinger <bisinger@kof.ethz.ch>
2024-01-18 18:10:02 +0000

    version 0.4.3

--- inst/doc/tstools.R
+++ inst/doc/tstools.R
@@ -42,10 +42,10 @@ missings <- ts(c(1, 2, 10, 3, 5, 6, NA, NA, 3, 2, 5, 3, 1, 1),
 tsplot(short)
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
-tsplot(ts1, ts2, auto_legend = F)
+tsplot(ts1, ts2, auto_legend = FALSE)
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
-tsplot(tslist, auto_legend = F)
+tsplot(tslist, auto_legend = FALSE)
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
 tsplot(list(
@@ -84,18 +84,18 @@ tsplot(list("KOF Barometer" = KOF$baro_point_fc),
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
 tsplot(tsb1, tsb2, tsb3,
-  left_as_bar = T,
-  auto_legend = F,
+  left_as_bar = TRUE,
+  auto_legend = FALSE,
   theme = init_tsplot_theme(bar_gap = 10)
 )
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
 tsl <- list(tsb1, tsb2, tsb3)
 tsplot(tsl,
-  left_as_bar = T,
+  left_as_bar = TRUE,
   manual_value_ticks_l = seq(-40, 100, by = 20),
-  auto_legend = F,
-  theme = init_tsplot_theme(sum_as_line = T)
+  auto_legend = FALSE,
+  theme = init_tsplot_theme(sum_as_line = TRUE)
 )
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
@@ -104,8 +104,8 @@ tsb2 <- ts(runif(30, 0, 50), start = c(2010, 1), frequency = 4)
 tsb3 <- ts(runif(30, 0, 50), start = c(2010, 1), frequency = 4)
 tsb4 <- ts(runif(30, -40, 10), start = c(2005, 1), frequency = 4)
 tsplot(tsb1, tsb2, tsb3, tsb4,
-  left_as_bar = T,
-  auto_legend = F
+  left_as_bar = TRUE,
+  auto_legend = FALSE
 )
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
@@ -113,9 +113,9 @@ tsb1 <- ts(runif(20, -30, 20), start = c(2010, 1), frequency = 12)
 tsb2 <- ts(runif(20, 0, 50), start = c(2010, 1), frequency = 12)
 tsb3 <- ts(runif(20, 0, 50), start = c(2010, 1), frequency = 12)
 tsplot(tsb1, tsb2, tsb3,
-  left_as_bar = T,
-  group_bar_chart = T,
-  auto_legend = F
+  left_as_bar = TRUE,
+  group_bar_chart = TRUE,
+  auto_legend = FALSE
 )
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
@@ -130,7 +130,7 @@ tsplot(tslist, left_as_band = TRUE)
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
 data(KOF)
 tsplot(KOF$kofbarometer,
-  tsr = KOF$reference, auto_legend = F
+  tsr = KOF$reference, auto_legend = FALSE
 )
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
@@ -140,8 +140,8 @@ tsb3 <- ts(runif(30, 0, 30), start = c(2010, 1), frequency = 4)
 tsr1 <- ts(runif(30, -4, 6), start = c(2010, 1), frequency = 4)
 tsplot(tsb1, tsb2, tsb3,
   tsr = tsr1,
-  left_as_bar = T,
-  auto_legend = F
+  left_as_bar = TRUE,
+  auto_legend = FALSE
 )
 
 ## -----------------------------------------------------------------------------
@@ -150,15 +150,15 @@ names(def_theme)
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
 tsplot(tsb1, tsb2, tsb3,
-  left_as_bar = T,
-  theme = init_tsplot_theme(highlight_window = T)
+  left_as_bar = TRUE,
+  theme = init_tsplot_theme(highlight_window = TRUE)
 )
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
-tt <- init_tsplot_theme(use_box = T)
+tt <- init_tsplot_theme(use_box = TRUE)
 tsplot(tsb1, tsb2, tsb3,
   tsr = tsr1,
-  left_as_bar = T,
+  left_as_bar = TRUE,
   theme = tt
 )
 
@@ -169,7 +169,7 @@ tsplot(tsb1, tsb2, tsb3,
 )
 
 ## ----fig.width = 7,fig.height=6-----------------------------------------------
-nt <- init_tsplot_theme(highlight_window = T)
+nt <- init_tsplot_theme(highlight_window = TRUE)
 nt$highlight_window_start <- c(2017, 1)
 nt$highlight_window_end <- c(2018, 1)
 tsplot(tsb1, tsb2,
@@ -179,8 +179,8 @@ tsplot(tsb1, tsb2,
 ## ----fig.width = 7,fig.height=6,echo=TRUE-------------------------------------
 tsplot(missings,
   theme = init_tsplot_theme(
-    NA_continue_line = T,
-    show_points = T
+    NA_continue_line = TRUE,
+    show_points = TRUE
   )
 )
 
@@ -189,7 +189,7 @@ tsplot(ts2)
 
 ## ----fig.width = 7,fig.height=6,echo=TRUE-------------------------------------
 tsplot(ts2,
-  theme = init_tsplot_theme(fill_year_with_nas = F)
+  theme = init_tsplot_theme(fill_year_with_nas = FALSE)
 )
 
 ## ----fig.width = 7,fig.height=6,echo=TRUE-------------------------------------
@@ -224,12 +224,12 @@ tsplot(KOF["kofbarometer"],
 tsplot("Some like\n loooong legends\n with so many words" = ts1)
 
 ## ----fig.width = 7,fig.height=6,echo=TRUE-------------------------------------
-tsplot(KOF[1], auto_legend = F)
+tsplot(KOF[1], auto_legend = FALSE)
 
 ## ----eval=FALSE---------------------------------------------------------------
 #  tsplot(KOF[1],
 #    output_format = "pdf",
-#    theme = init_tsplot_print_theme(output_wide = T)
+#    theme = init_tsplot_print_theme(output_wide = TRUE)
 #  )
 
 ## ----eval=FALSE---------------------------------------------------------------
@@ -239,17 +239,17 @@ tsplot(KOF[1], auto_legend = F)
 ## ----eval=FALSE---------------------------------------------------------------
 #  write_ts(KOF, file.path(tempdir(), "test_export_wide_trans"),
 #    "csv",
-#    wide = T,
-#    transpose = T
+#    wide = TRUE,
+#    transpose = TRUE
 #  )
 
-## ----eval=F,echo=T------------------------------------------------------------
+## ----eval=FALSE,echo=TRUE-----------------------------------------------------
 #  init_tsplot_theme(
-#    legend_all_left = T,
+#    legend_all_left = TRUE,
 #    legend_col = 1
 #  )
 
-## ----eval=F,echo=T------------------------------------------------------------
+## ----eval=FALSE,echo=TRUE-----------------------------------------------------
 #  init_tsplot_theme(
 #    band_fill_color = c("#FF0000", "#00FF00"),
 #    line_colors = c("#FF0000", "#00FF00"),
